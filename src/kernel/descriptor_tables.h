147b90a7d68169b88115a51b96b2fd2c82bc66d0
Tharindu Hasthika <tharindubathigama@gmail.com>
2018-05-04 22:01:49 +0530

    cleaning

--- src/kernel/descriptor_tables.h
+++
@@ -1,9 +0,0 @@
-/* header files to handle GDT, IDT, and other descriptor tables that the processor needs */
-
-#ifndef _DESCRIPTOR_TABLES
-#define _DESCRIPTOR_TABLES
-
-void init_descriptor_tables();
-
-
-#endif // _DESCRIPTOR_TABLES
